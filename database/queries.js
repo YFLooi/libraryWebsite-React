709a23f7a110697206ab878b88c6ebc481a64af7
YF Looi <40136494+YFLooi@users.noreply.github.com>
2019-04-29 20:43:09 +0800

    borrowing system draft

---
+++ database/queries.js
@@ -0,0 +1,130 @@
+﻿/*sets up the configuration of your PostgreSQL connection*/
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'booklibrary',
+  password: 'x',
+  port: 5432,
+})
+
+/*Selects all items in the 'newarrival' table*/
+const getNewArrivals = (request, response) => {
+    pool.query('SELECT * FROM newarrival ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+/*Basic search*/
+const getBasicSearch = (request, response) => {
+    let input = request.params.input;
+    console.log(input);
+    
+    pool.query('SELECT * FROM catalog WHERE title ~* $1 OR author ~*$1', [input], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+/*Adv search*/
+const getAdvSearch = (request, response) => {
+    let advTitle = request.params.advTitle;
+    let condTitAuth = request.params.condTitAuth;
+    let advAuthor = request.params.advAuthor;
+   
+    
+    let condAuthYr = request.params.condAuthYr;
+    let advYearStart = request.params.advYearStart;
+
+    let advYearEnd = request.params.advYearEnd;
+    let condYrPub = request.params.condYrPub;
+    let advPublisher = request.params.advPublisher;
+    let condPubSynp = request.params.condPubSynp;
+    let advSynopsis = request.params.advSynopsis; /** */
+    console.log("Query received: Title:"+advTitle+" "+condTitAuth+" Author:"+advAuthor+" "
+    +condAuthYr+" Where YearStart:"+advYearStart+" until YearEnd:"+advYearEnd+" "+condYrPub+" Publisher:"+advPublisher+" "+condPubSynp+" Synopsis text:"+advSynopsis);
+    
+    /*Inserting AND/OR must use graves (``) for string interpolation. Using ("++") causes the SQL query 
+    to bug out
+    How to insert a range of years for the query?*/
+    const text = `SELECT * FROM catalog WHERE title ~* $1 ${condTitAuth} author ~* $2
+    ${condAuthYr} (year >=$3 AND year <=$4) ${condYrPub} publisher ~* $5 ${condPubSynp}
+    synopsis ~* $6`;
+    const values = [advTitle, advAuthor, advYearStart, advYearEnd, advPublisher, advSynopsis]
+    pool.query(text, values, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+        
+    })
+    
+}
+
+/* 
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createUser = (request, response) => {
+    const { name, email } = request.body
+
+    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${result.insertId}`)
+    })
+}
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, email } = request.body
+
+    pool.query(
+        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+        [name, email, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
+*/
+
+/*'module.export' allows multiple functions to be exported at the same time! No need to declare
+ one by one (ie export const deleteUser(){}).
+ With ES6 syntax, its getUsers instead of getUsers:getUsers*/
+module.exports = {
+    getNewArrivals,
+    getBasicSearch,
+    getAdvSearch,
+    /*Trailing comma ok*/
+}
+
+
+
---
+++ src/Components/App.jsx
@@ -0,0 +1,355 @@
+import React from 'react';
+
+//Controls positioning of individual columns and applies general styling
+import './App.css';
+
+//imports React components to render()
+import Header from "./Header/Header.jsx";
+import NewArrivals from "./NewArrivals/NewArrivals.jsx";
+
+// App.js value={this.state.input}
+export default class App extends React.Component {
+	constructor(props) {
+        super(props);	
+        
+        /*Conditionals need to be pre-initialised, otherwise will be blank if user does not change*/
+        this.state = {
+            newarrivals: [],
+            isLoaded: false,
+            
+            basicInput: "",
+
+            advTitle: "",
+            condTitAuth:'OR',
+            advAuthor: "",
+            condAuthYr:'OR',
+            advYearStart: "",
+            advYearEnd: "",
+            condYrPub: "OR",
+            advPublisher: "",
+            condPubSynp:'OR',
+            advSynopsis: "",
+
+            borrowCart: []
+        }
+        
+        this.handleBasicSearchChange = this.handleBasicSearchChange.bind(this);
+        this.handleBasicSearchSubmit = this.handleBasicSearchSubmit.bind(this);
+        
+        this.handleAdvSearchChange = this.handleAdvSearchChange.bind(this);
+        this.handleAdvSearchSubmit = this.handleAdvSearchSubmit.bind(this);
+        
+        this.borrowRequest = this.borrowRequest.bind(this);
+        this.checkbasicInput = this.checkbasicInput.bind(this);
+        this.checkadvInput = this.checkadvInput.bind(this);
+        this.checkborrowCart = this.checkborrowCart.bind(this);
+    }
+	componentDidMount(){
+        let that = this; //Prevents 'this' from being undefined
+
+        /*Fetches the data on page load for the New Arrivals slideshow*/
+        fetch('http://localhost:3005/NewArrivals', {mode:'cors'})
+            //Here we chain 2 promise functions: The first fetches data (response), the second examines text in response (data)
+            .then(function(response){
+                return response.json()
+                //Examines data in response
+                .then(function(data){
+                    that.setState({
+                        newarrivals: data,
+                        isNewArrivalsLoaded: true
+                    })
+                })
+            })  
+            .catch(function(error){
+                console.log('Request failed', error)
+            })
+    }
+    handleBasicSearchChange(event){
+        event.preventDefault();
+        this.setState({
+			basicInput: event.target.value
+        });
+    }
+    handleBasicSearchSubmit(event) { 
+        event.preventDefault();
+        const basicInput = this.state.basicInput;
+
+        if(basicInput !== ""){
+            fetch("http://localhost:3005/BasicSearch/"+basicInput, {mode:'cors'})
+                //Here we chain 2 promise functions: The first fetches data (response), the second examines text in response (data)
+                .then(function(response){
+                    return response.json()
+                    .then(function(data){
+                        console.log(data);
+                    })
+                })  
+                .catch(function(error){
+                    console.log('Request failed', error)
+                })
+        } else {
+            console.log("Blank query made. No query submitted");
+        }
+    }
+    handleAdvSearchChange(event){ 
+        /*When called, it looks for the state with the same name 
+        as the <input> box and updates it as =event.target.value 
+        (value or content of input box)*/
+        /*Only works if <input> has same name as state!*/
+        this.setState({ 
+            [event.target.name]: event.target.value 
+        })   
+    }
+    handleAdvSearchSubmit(event){
+        event.preventDefault();
+
+        /*JSON parse cannot accept blank strings, "". The if-else here inserts string "null"
+        if it detects the submitted state is ""*/
+        const advTitle = this.state.advTitle === "" ? "null" : this.state.advTitle;
+        const condTitAuth = this.state.condTitAuth 
+        const advAuthor = this.state.advAuthor === "" ? "null" : this.state.advAuthor;
+        const condAuthYr = this.state.condAuthYr;
+        const advYearStart = this.state.advYearStart === "" ? "null" : this.state.advYearStart;
+        const advYearEnd = this.state.advYearEnd === "" ? "null" : this.state.advYearEnd;
+        const condYrPub = this.state.condYrPub;
+        const advPublisher = this.state.advPublisher === "" ? "null" : this.state.advPublisher;
+        const condPubSynp = this.state.condPubSynp;
+        const advSynopsis = this.state.advSynopsis === "" ? "null" : this.state.advSynopsis;
+
+        console.log(advTitle+condTitAuth+advAuthor+condAuthYr+advYearStart+" to "+advYearEnd+condYrPub+advPublisher+condPubSynp+advSynopsis)
+
+        /*Search occurs as long as one advSearch parameter is not a "null" string*/
+        if(advTitle === "null" &&  advAuthor === "null" && advPublisher === "null" && 
+        advYearStart === "null" && advYearEnd === "null" && advPublisher === "null" 
+        && advSynopsis === "null"){
+            console.log("Blank query made. No query submitted");
+        } else {
+            fetch("http://localhost:3005/AdvSearch/"+advTitle+"/"+condTitAuth+"/"+advAuthor+"/"+condAuthYr+
+            "/"+advYearStart+"/"+advYearEnd+"/"+condYrPub+"/"+advPublisher+"/"+condPubSynp+"/"+advSynopsis
+            ,{mode:'cors'})
+                //Here we chain 2 promise functions: The first fetches data (response), the second examines text in response (data)
+                .then(function(response){
+                    return response.json()
+                    .then(function(data){
+                        console.log(data);
+                    })
+                })  
+                .catch(function(error){
+                    console.log('Request failed', error)
+                })
+        }
+    }
+    borrowRequest(id){
+        //'id' here is the book id
+        let buttonText = document.getElementById("borrow."+id).innerHTML;
+        let cart = this.state.borrowCart;
+        console.log(buttonText);
+
+        if(buttonText === "Borrow"){
+            /*This method adds each new book id to the end of the existing array immutably*/
+            this.setState({
+                borrowCart: [...cart, id]
+            })
+            document.getElementById("borrow."+id).innerHTML = "Cancel";
+        }else if(buttonText === "Cancel"){
+            //Remove added book id from borrowCart
+            let targetPosition = cart.indexOf(id);
+            console.log("Target of removal position: "+targetPosition);
+
+            //Prevents splice if id to remove not in cart 
+            if(targetPosition !== -1){
+                /*Removes item at targetPosition. If we set const new = cart.splice(), 
+                "const new" has a value = the removed item*/
+                cart.splice(targetPosition,1);                 
+                this.setState({
+                    borrowCart: [...cart] //Keep state immutable with spread syntax!
+                })
+            }
+            document.getElementById("borrow."+id).innerHTML = "Borrow";       
+        }
+    }
+    checkbasicInput(){
+        console.log("Basic input query stored: "+this.state.basicInput);
+    }
+    checkadvInput(){
+        console.log("Advanced input query stored: ");
+        console.log("Title: "+this.state.advTitle);
+        console.log("Title-Author conditional: "+this.state.condTitAuth);
+        console.log("Auth: "+this.state.advAuthor);
+        console.log("Author-Yr conditional: "+this.state.condAuthYr);
+        console.log("Yr start: "+this.state.advYearStart);
+        console.log("Yr end: "+this.state.advYearEnd);
+        console.log("Yr-Publisher conditional: "+this.state.condYrPub);
+        console.log("Publ: "+this.state.advPublisher);
+        console.log("Publisher-synopsis conditional: "+this.state.condPubSynp);
+        console.log("Synp short: "+this.state.advSynopsis);
+    }
+    checkborrowCart(){
+        console.log("Book ids in cart: "+this.state.borrowCart);
+    }
+
+	render() {	
+        if (this.state.isNewArrivalsLoaded === true){
+            return (
+                <div>
+                    <Header 
+                        basicInput={this.state.basicInput} 
+                        handleBasicSearchChange={this.handleBasicSearchChange} 
+                        handleBasicSearchSubmit = {this.handleBasicSearchSubmit}
+                    />
+                
+                    <NewArrivals newarrivals={this.state.newarrivals}/>
+
+                    <h1>Advanced search</h1>
+                    <form name="advsearch" onSubmit={this.handleAdvSearchSubmit}>
+                        Search books matching the following criteria...
+                        <p>
+                            Title:&nbsp;
+                            <input
+                                type="text"
+                                name="advTitle"
+                                className="title"
+                                value={this.state.advTitle}
+                                onChange = {this.handleAdvSearchChange}
+                                onSubmit = {this.handleAdvSearchSubmit}
+                                placeholder="'Robin' or 'Robin Hood'"
+                                autoComplete="on"
+                                style={{borderColor:"none"}}
+                            />
+                            
+                        </p>
+                        <div onChange={this.handleAdvSearchChange}>
+                            <input type="radio" name="condTitAuth" value="AND" id="ANDcondTitAuth"/><label htmlFor="ANDcondTitAuth">AND</label>
+                            <input type="radio" name="condTitAuth" value="OR" id="ORcondTitAuth" defaultChecked/><label htmlFor="ORcondTitAuth">OR</label>
+                        </div>
+                        <p>
+                            Author:&nbsp;
+                            <input
+                            type="text"
+                            name="advAuthor"
+                            className="author"
+                            value={this.state.advAuthor}
+                            onChange = {this.handleAdvSearchChange}
+                            onSubmit = {this.handleAdvSearchSubmit}
+                            placeholder="'Jane' or 'Jane Austen'"
+                            autoComplete="on"
+                            style={{borderColor:"none"}}
+                        />
+                        </p>
+                        <div onChange={this.handleAdvSearchChange}>
+                            <input type="radio" name="condAuthYr" value="AND" id="ANDcondAuthYr"/><label htmlFor="ANDcondAuthYr">AND</label>
+                            <input type="radio" name="condAuthYr" value="OR" id="ORcondAuthYr" defaultChecked/><label htmlFor="ORcondAuthYr">OR</label>
+                        </div>
+                        <p>
+                            Year range:&nbsp;
+                            <input
+                                type="text"
+                                name="advYearStart"
+                                className="yearstart"
+                                value={this.state.advYearStart}
+                                onChange = {this.handleAdvSearchChange}
+                                onSubmit = {this.handleAdvSearchSubmit}
+                                placeholder="From..."
+                                autoComplete="on"
+                                style={{borderColor:"none"}}
+                            />
+                            &nbsp;-&nbsp; 
+                            <input
+                                type="text"
+                                name="advYearEnd"
+                                className="yearend"
+                                value={this.state.advYearEnd}
+                                onChange = {this.handleAdvSearchChange}
+                                onSubmit = {this.handleAdvSearchSubmit}
+                                placeholder="To..."
+                                autoComplete="on"
+                                style={{borderColor:"none"}}
+                            />
+                        </p>
+                        <div onChange={this.handleAdvSearchChange}>
+                            <input type="radio" name="condYrPub" value="AND" id="ANDcondYrPub"/><label htmlFor="ANDcondYrPub">AND</label>
+                            <input type="radio" name="condYrPub" value="OR" id="ORcondYrPub" defaultChecked/><label htmlFor="ORcondYrPub">OR</label>
+                        </div>
+                        <p>
+                            Publisher:&nbsp;
+                            <input
+                                type="text"
+                                name="advPublisher"
+                                className="publisher"
+                                value={this.state.advPublisher}
+                                onChange = {this.handleAdvSearchChange}
+                                onSubmit = {this.handleAdvSearchSubmit}
+                                placeholder="'Penguin Books'"
+                                autoComplete="on"
+                                style={{borderColor:"none"}}
+                            />
+                        </p>
+                        <div onChange={this.handleAdvSearchChange}>
+                            <input type="radio" name="condPubSynp" value="AND" id="ANDcondPubSynp"/><label htmlFor="ANDcondPubSynp">AND</label>
+                            <input type="radio" name="condPubSynp" value="OR" id="ORcondPubSynp" defaultChecked/><label htmlFor="ORcondPubSynp">OR</label>
+                        </div>
+                        <p>
+                            Synopsis key words:&nbsp;
+                            <input
+                                type="text"
+                                name="advSynopsis"
+                                className="synopsis"
+                                value={this.state.advSynopsis}
+                                onChange = {this.handleAdvSearchChange}
+                                onSubmit = {this.handleAdvSearchSubmit}
+                                placeholder="Retrieves partial matches"
+                                autoComplete="on"
+                                style={{borderColor:"none"}}
+                            /> 
+                        </p>
+                       
+
+                        {/* 
+                        <input type="checkbox" id="safe" name="safe" value="on" defaultChecked="true"/><label htmlFor="safe">SafeSearch</label>
+                        */}    
+                        <button className="searchbutton" type="submit"></button>
+                        
+                    </form>          
+                    
+                    <p></p>
+
+                    <div><u>Mock search results</u>
+                        <div >
+                            <span>
+                                The Elder Scrolls: Do you even?
+                                <button id="borrow.1" onClick={(event) => {this.borrowRequest("1")}}>Borrow</button>
+                            </span>
+                        </div>
+                        <div >
+                            <span>
+                                The Lusty Argonian Maid: Scaly Hentai
+                                <button id="borrow.2" onClick={(event) => {this.borrowRequest("2")}}>Borrow</button>
+                            </span>
+                        </div>
+                        <div >
+                            <span>
+                                Big book of poison
+                                <button id="borrow.3" onClick={(event) => {this.borrowRequest("3")}}>Borrow</button>
+                            </span>
+                        </div>
+                        <div >
+                            <span>
+                                Sorenova: Him, her, it?
+                                <button id="borrow.4" onClick={(event) => {this.borrowRequest("4")}}>Borrow</button>
+                            </span>
+                        </div>
+                    </div>
+                 
+                    <button onClick={this.checkbasicInput}>Check basic search query stored in state</button>
+                    <button onClick={this.checkadvInput}>Check adv search query stored in state</button>
+                    <button onClick={this.handleAdvSearchSubmit}>test search query send</button>
+                    <button onClick={this.checkborrowCart}>Check book id-s in cart</button>
+                    
+			    </div>
+            )
+        } else {
+            return (
+                <div>Error 404: Database cannot be loaded</div>
+            );
+        }
+	}
+}
